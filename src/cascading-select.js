48d15a9850f8d27e3fe49e6e72bd890dc38df160
Søren Birkemeyer <sb@webfactory.de>
2025-01-09 22:03:39 +0000

    Vereinfache Setzen des initiallySelectedDependentOptionValue

--- src/cascading-select.js
+++ src/cascading-select.js
@@ -38,9 +38,7 @@ class CascadingSelect extends HTMLElement {
         this.dependentSelectId = this.parentSelect.getAttribute('data-dependent-id');
         this.dependentSelect = this.querySelector(`#${this.dependentSelectId}`);
         this.dependentLabel = this.querySelector(`[for=${this.dependentSelectId}]`);
-        this.initiallySelectedDependentOptionValue = this.dependentSelect.options[this.dependentSelect.selectedIndex].value;
-
-        console.log(this.initiallySelectedDependentOptionValue);
+        this.initiallySelectedDependentOptionValue = this.dependentSelect.value;
 
         this.parentSelect.addEventListener('change', this.updateDependentSelect.bind(this));
 
@@ -77,7 +75,6 @@ class CascadingSelect extends HTMLElement {
             this.showDependent();
         } else {
             this.setInactiveDependentVisibility();
-            this.dependentSelect.options[0].selected = true;
         }
     }
 
